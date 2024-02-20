f20f136c8155837ab3cfed914eae9b53cf0c2916
Lucas <lucasver1799@gmail.com>
2024-02-20 12:02:48 -0300

    Starting a new webpage

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res){
     res.send("Hello World")
 });
 
+app.get("/oi", (req, res) => {
+    res.send("Olá mundo")
+})
+
 app.listen(port, () => {
     console.log("Rodando na porta " + port)
 })
